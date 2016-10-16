f81b00527decd9cba47254f077abfc982b52773f
Onduril <Onduril@free.fr>
2016-10-17 01:25:20 +0200

    first draft using c++17 non type auto template parameter

--- unique_ptr_deleter_size_trick.cpp
+++ unique_ptr_deleter_size_trick.cpp
@@ -5,6 +5,24 @@
 // FunctionFunctor : A functor class calling the given function.
 // Can be use as base for empty base optimization (ebo).
 
+#if __cplusplus > 201402L
+// c++17 : use auto for non type template parameter
+// 
+
+template <auto f>
+struct FunctionFunctor
+{
+    template <typename Args>
+    auto operator()(Args&&... args)
+    {
+        return f(std::forward<Args>(args)...);
+    }
+};
+
+// Macro for before c++17
+#define FUNCTION_FUNCTOR(f) FunctionFunctor<f>
+
+#else
 template <typename F, F f> // or auto F in c++17
 struct FunctionFunctor;
 
@@ -27,11 +45,10 @@ struct FunctionFunctor<R(*)(Args...), f>
     }
 };
 
-
-
 // macro to avoid repeating f.
 // Probably that C++17 auto non type template will improve it.
 #define FUNCTION_FUNCTOR(f) FunctionFunctor<decltype(f), f>
+#endif
 
 void foo0() {}
 void foo1(int) {}
